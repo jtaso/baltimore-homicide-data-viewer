bfe3d0e110d12ef3a90cb1c308ec559fe3d138e3
Jamie T <jamietsao42@gmail.com>
2020-12-30 04:15:10 +0900

    install Postgres client for Node.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,28 @@
 
 const express = require('express');
 
-// Constants
+// requiring postgres client module
+const { Client } = require('pg');
+const client = new Client({
+    user: 'postgres',
+    host: 'database',
+    database: 'postgres',
+    password: 'pw123',
+    port: 5432,
+});
+client.connect();
+
+// promise
+client
+    .query('SELECT * FROM homicide_info')
+    .then(res => console.log(res.rows[0]))
+    .catch(e => console.error(e.stack));
+
+// constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+// app
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World');
